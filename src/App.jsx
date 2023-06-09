76bb140b8c39383165d22ba51e2b60851485c5ac
MemoGomz <guillermogomezmaya@gmail.com>
2023-06-09 17:31:53 -0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export function App() {
+    return (
+      <div className="container">
+        <article className="centered">
+          <strong>Expensive Control</strong>
+        </article>
+        <article>
+          <div>
+            <input type="number" className="custom-input" placeholder="Total" />
+          </div>
+        </article>
+      </div>
+    );
+  }
